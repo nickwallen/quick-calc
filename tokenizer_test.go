02f2e99ff78ad6dad2e6a14dbdc50df1e2e9154b
Nick Allen <nickallen@apache.org>
2020-04-19 14:50:17 -0400

    Split out tokenizer and tokens test cases. Added test case for Tokens()

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -6,74 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDecimals(t *testing.T) {
-	expected := []Token{NumberToken.Of("22"), EOFToken.Of("")}
-	expect(t, expected, New("22").Tokens())
-	expect(t, expected, New("  22").Tokens())
-	expect(t, expected, New("22  ").Tokens())
-}
-
-func TestBadDecimal(t *testing.T) {
-	expected := []Token{ErrorToken.Of("expected number, but got \"2A\"")}
-	expect(t, expected, New("2A").Tokens())
-	expect(t, expected, New("  2A").Tokens())
-	expect(t, expected, New("2A  ").Tokens())
-}
-
-func TestHexaDecimal(t *testing.T) {
-	expected := []Token{NumberToken.Of("0xAF"), EOFToken.Of("")}
-	expect(t, expected, New("0xAF").Tokens())
-	expect(t, expected, New("  0xAF").Tokens())
-	expect(t, expected, New("0xAF  ").Tokens())
-}
-
-func TestBadHexaDecimal(t *testing.T) {
-	expected := []Token{ErrorToken.Of("expected number, but got \"0xG\"")}
-	expect(t, expected, New("0xG2").Tokens())
-	expect(t, expected, New("  0xG2").Tokens())
-	expect(t, expected, New("0xG2  ").Tokens())
-}
-
-func TestFloats(t *testing.T) {
-	expected := []Token{NumberToken.Of("2.22"), EOFToken.Of("")}
-	expect(t, expected, New("2.22").Tokens())
-	expect(t, expected, New("  2.22").Tokens())
-	expect(t, expected, New("2.22  ").Tokens())
-}
-
-func TestExpNotation(t *testing.T) {
-	expected := []Token{NumberToken.Of("2E10"), EOFToken.Of("")}
-	expect(t, expected, New("2E10").Tokens())
-	expect(t, expected, New("  2E10").Tokens())
-	expect(t, expected, New("2E10  ").Tokens())
-}
-
-func TestPlus(t *testing.T) {
-	expected := []Token{NumberToken.Of("2"), PlusToken.Of("+"), NumberToken.Of("2"), EOFToken.Of("")}
-	expect(t, expected, New("2 + 2").Tokens())
-	expect(t, expected, New("2+2").Tokens())
-	expect(t, expected, New("   2 + 2").Tokens())
-	expect(t, expected, New("2+2   ").Tokens())
-}
-
-func TestManyPlus(t *testing.T) {
-	expected := []Token{NumberToken.Of("2"), PlusToken.Of("+"), ErrorToken.Of("expected number, but got \"+\"")}
-	expect(t, expected, New("2 ++ 2").Tokens())
-}
-
-func TestMinus(t *testing.T) {
-	expected := []Token{NumberToken.Of("2"), MinusToken.Of("-"), NumberToken.Of("2"), EOFToken.Of("")}
-	expect(t, expected, New("2 - 2").Tokens())
-	expect(t, expected, New("2-2").Tokens())
-	expect(t, expected, New("   2 - 2").Tokens())
-	expect(t, expected, New("2-2   ").Tokens())
-}
-
-func TestManyMinus(t *testing.T) {
-	expected := []Token{NumberToken.Of("2"), MinusToken.Of("-"), ErrorToken.Of("expected number, but got \"-\"")}
-	expect(t, expected, New("2 --  2").Tokens())
-}
-
 func TestNextToken(t *testing.T) {
 	tok := New("2 + 2")
 	assert.Equal(t, NumberToken.Of("2"), tok.NextToken())
@@ -82,11 +14,10 @@ func TestNextToken(t *testing.T) {
 	assert.Equal(t, EOFToken.Of(""), tok.NextToken())
 }
 
-func expect(t *testing.T, expected []Token, tokens chan Token) {
-	// pul the actual tokens off the channel
-	actuals := make([]Token, 0)
-	for token := range tokens {
-		actuals = append(actuals, token)
-	}
-	assert.ElementsMatch(t, expected, actuals)
+func TestTokens(t *testing.T) {
+	tok := New("2 + 2")
+	assert.Equal(t, NumberToken.Of("2"), <-tok.Tokens())
+	assert.Equal(t, PlusToken.Of("+"), <-tok.Tokens())
+	assert.Equal(t, NumberToken.Of("2"), <-tok.Tokens())
+	assert.Equal(t, EOFToken.Of(""), <-tok.Tokens())
 }
---
+++ tokens_test.go
@@ -0,0 +1,96 @@
+package toks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecimals(t *testing.T) {
+	expected := []Token{NumberToken.Of("22"), EOFToken.Of("")}
+	inputs := []string{"22", "  22", "22    "}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestBadDecimal(t *testing.T) {
+	expected := []Token{ErrorToken.Of("expected number, but got \"2A\"")}
+	inputs := []string{"2A", "   2A", "2A   "}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestHexaDecimal(t *testing.T) {
+	expected := []Token{NumberToken.Of("0xAF"), EOFToken.Of("")}
+	inputs := []string{"0xAF", "   0xAF", "0xAF   "}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestBadHexaDecimal(t *testing.T) {
+	expected := []Token{ErrorToken.Of("expected number, but got \"0xG\"")}
+	inputs := []string{"0xG2", "   0xG2", "0xG2   "}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestFloats(t *testing.T) {
+	expected := []Token{NumberToken.Of("2.22"), EOFToken.Of("")}
+	inputs := []string{"2.22", "   2.22", "2.22   "}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestExpNotation(t *testing.T) {
+	expected := []Token{NumberToken.Of("2E10"), EOFToken.Of("")}
+	inputs := []string{"2E10", "   2E10", "2E10   "}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestPlus(t *testing.T) {
+	expected := []Token{NumberToken.Of("2"), PlusToken.Of("+"), NumberToken.Of("2"), EOFToken.Of("")}
+	inputs := []string{"2 + 2", "   2+2", "   2 +   2   ", "2+2"}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestManyPlus(t *testing.T) {
+	expected := []Token{NumberToken.Of("2"), PlusToken.Of("+"), ErrorToken.Of("expected number, but got \"+\"")}
+	inputs := []string{"2 ++ 2", "   2++2", "   2++   2   ", "2++2"}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestMinus(t *testing.T) {
+	expected := []Token{NumberToken.Of("2"), MinusToken.Of("-"), NumberToken.Of("2"), EOFToken.Of("")}
+	inputs := []string{"2 - 2", "   2-2", "   2 -   2   ", "2-2"}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func TestManyMinus(t *testing.T) {
+	expected := []Token{NumberToken.Of("2"), MinusToken.Of("-"), ErrorToken.Of("expected number, but got \"-\"")}
+	inputs := []string{"2 -- 2", "   2--2", "   2 --   2   ", "2--2"}
+	for _, input := range inputs {
+		expect(t, expected, New(input).Tokens())
+	}
+}
+
+func expect(t *testing.T, expected []Token, tokens chan Token) {
+	// pul the actual tokens off the channel
+	actuals := make([]Token, 0)
+	for token := range tokens {
+		actuals = append(actuals, token)
+	}
+	assert.ElementsMatch(t, expected, actuals)
+}
