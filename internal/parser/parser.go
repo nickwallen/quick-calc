0ecde256289158d17758c973da79331d154600af
Nick Allen <nickallen@apache.org>
2020-04-22 10:48:43 -0400

    No longer need the parser data structure. Just pass around a tokenReader

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -6,13 +6,6 @@ import (
 	"strconv"
 )
 
-// TODO no longer need to have this type; just pass around a tokenReader
-// parser Parses input text and outputs an Expression.
-type parser struct {
-	// the channel from which tokens can be read
-	reader tokenReader
-}
-
 // the interface used by the parser to read tokens
 type tokenReader interface {
 	ReadToken() (tokens.Token, error)
@@ -23,8 +16,7 @@ func Parse(reader tokenReader) (Expression, error) {
 	var expression Expression
 
 	// an expression should start with an Amount like '23 pounds'
-	parser := &parser{reader: reader}
-	amount1, err := parser.expectAmount()
+	amount1, err := expectAmount(reader)
 	if err != nil {
 		return expression, err
 	}
@@ -37,10 +29,10 @@ func Parse(reader tokenReader) (Expression, error) {
 	// the next token defines if this is an operation or a conversion
 	switch token.TokenType {
 	case tokens.Plus, tokens.Minus, tokens.Multiply, tokens.Divide:
-		return parser.expectOperation(amount1, token.TokenType)
+		return expectOperation(reader, amount1, token.TokenType)
 
 	case tokens.In:
-		return parser.expectConversion(amount1)
+		return expectConversion(reader, amount1)
 
 	case tokens.EOF:
 		// all tokens have been consumed, all we have is the first amount
@@ -52,17 +44,17 @@ func Parse(reader tokenReader) (Expression, error) {
 	}
 }
 
-func (parser *parser) expectConversion(amount1 Amount) (Expression, error) {
+func expectConversion(reader tokenReader, amount1 Amount) (Expression, error) {
 	var expression Expression
 
 	// expect the Units to convert to
-	units, err := parser.expectUnits()
+	units, err := expectUnits(reader)
 	if err != nil {
 		return expression, err
 	}
 
 	// expect EOF
-	_, err = parser.nextToken(tokens.EOF)
+	_, err = nextToken(reader, tokens.EOF)
 	if err != nil {
 		return expression, err
 	}
@@ -71,17 +63,17 @@ func (parser *parser) expectConversion(amount1 Amount) (Expression, error) {
 	return unitConverterOf(amount1, units), nil
 }
 
-func (parser *parser) expectOperation(amount1 Amount, operator tokens.TokenType) (Expression, error) {
+func expectOperation(reader tokenReader, amount1 Amount, operator tokens.TokenType) (Expression, error) {
 	// to this point, we've already seen... operand1 +
 	var expression Expression
 
 	// expect the second operand
-	amount2, err := parser.expectAmount()
+	amount2, err := expectAmount(reader)
 	if err != nil {
 		return expression, err
 	}
 
-	token, err := parser.reader.ReadToken()
+	token, err := reader.ReadToken()
 	if err != nil {
 		return expression, err
 	}
@@ -94,13 +86,13 @@ func (parser *parser) expectOperation(amount1 Amount, operator tokens.TokenType)
 
 	case tokens.In:
 		// the Units have been specified for expressions like '2 kg + 2 g in grams'
-		units, err := parser.expectUnits()
+		units, err := expectUnits(reader)
 		if err != nil {
 			return expression, err
 		}
 
 		// expect EOF
-		_, err = parser.nextToken(tokens.EOF)
+		_, err = nextToken(reader, tokens.EOF)
 		if err != nil {
 			return expression, err
 		}
@@ -113,9 +105,9 @@ func (parser *parser) expectOperation(amount1 Amount, operator tokens.TokenType)
 	}
 }
 
-func (parser *parser) expectAmount() (Amount, error) {
+func expectAmount(reader tokenReader) (Amount, error) {
 	var amount Amount
-	token, err := parser.nextToken(tokens.Number)
+	token, err := nextToken(reader, tokens.Number)
 	if err != nil {
 		return amount, err
 	}
@@ -124,7 +116,7 @@ func (parser *parser) expectAmount() (Amount, error) {
 	if err != nil {
 		return amount, err
 	}
-	units, err := parser.expectUnits()
+	units, err := expectUnits(reader)
 	if err != nil {
 		return amount, err
 	}
@@ -132,8 +124,8 @@ func (parser *parser) expectAmount() (Amount, error) {
 	return expression, nil
 }
 
-func (parser *parser) expectUnits() (units Units, err error) {
-	token, err := parser.nextToken(tokens.Units)
+func expectUnits(reader tokenReader) (units Units, err error) {
+	token, err := nextToken(reader, tokens.Units)
 	if err != nil {
 		return units, err
 	}
@@ -144,9 +136,9 @@ func (parser *parser) expectUnits() (units Units, err error) {
 	return units, nil
 }
 
-func (parser *parser) nextToken(expected tokens.TokenType) (token tokens.Token, err error) {
+func nextToken(reader tokenReader, expected tokens.TokenType) (token tokens.Token, err error) {
 	// get the next token
-	token, err = parser.reader.ReadToken()
+	token, err = reader.ReadToken()
 	if err != nil {
 		return token, err
 	}
